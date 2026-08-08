e98ae2d2e7ea49f23df8e99394f18a495466f770
harishks7770 <harishkumartherockstar@gmail.com>
2026-08-08 19:41:38 +0530

    Committing 1 item from workspace 7e55f29a-3024-4551-8515-f4406d0cda2b

--- nb_transform_data.Notebook/notebook-content.py
+++ nb_transform_data.Notebook/notebook-content.py
@@ -8,14 +8,9 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "83ee311b-dbf5-4b77-9840-a969c0281a49",
-# META       "default_lakehouse_name": "lh_sales_dev",
-# META       "default_lakehouse_workspace_id": "7e55f29a-3024-4551-8515-f4406d0cda2b",
-# META       "known_lakehouses": [
-# META         {
-# META           "id": "83ee311b-dbf5-4b77-9840-a969c0281a49"
-# META         }
-# META       ]
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": "",
+# META       "known_lakehouses": []
 # META     }
 # META   }
 # META }
