6624c7aedf79446e6e89a7b936cb7d42ff379f11
harishks7770 <harishkumartherockstar@gmail.com>
2026-08-08 19:29:43 +0530

    Committing 1 item from workspace 7e55f29a-3024-4551-8515-f4406d0cda2b

--- nb_transform_data.Notebook/notebook-content.py
+++ nb_transform_data.Notebook/notebook-content.py
@@ -6,13 +6,34 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "83ee311b-dbf5-4b77-9840-a969c0281a49",
+# META       "default_lakehouse_name": "lh_sales_dev",
+# META       "default_lakehouse_workspace_id": "7e55f29a-3024-4551-8515-f4406d0cda2b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "83ee311b-dbf5-4b77-9840-a969c0281a49"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
 
 
 # METADATA ********************
